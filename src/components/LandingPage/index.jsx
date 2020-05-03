80267a5cbff2e7cad02e0049ed86f1757d79ff31
Juhana Viitamo <jviitamo@gmail.com>
2020-05-03 22:06:58 +0300

    added typing text to the landing page

--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 import "./styles.css";
+import Typing from 'react-typing-animation';
 
 //pitäisikö tänne lisätä esimerkiksi jotain automaattisesti näkyviin tulevaa sisältöä feidattuna, esimerkiksi se kirjoitettu tekstipätkä tai uutisotsikoita?
 //lisätään myös nuoli alaspäin, jolla voi klikata seuraavaan osioon ja häviää kun scrollaa alaspäin
 
 
+
 class LandingPage extends Component {
-  
+
 
   onClick = index => this.props.fullpage.fullpageApi.moveTo(index);
   
   render() {
     return (
       <section className={"landing-page section"}>
-        <h1>"Älä patoa luonnon virtaa"</h1>
+        <Typing speed={150} className="head">"Älä patoa luonnon virtaa"</Typing>
         <div class="arrow" onClick={() => this.onClick(3)}>
           <span></span>
           <span></span>
