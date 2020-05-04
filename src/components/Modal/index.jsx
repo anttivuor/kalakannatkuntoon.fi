e28904c207df4a6077d1f92f0912b38afb45f3e0
Antti Vuorenmaa <antti@vertics.co>
2020-05-04 17:09:18 +0300

    Added modal component

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Modal = (props) => (
+    <div className={`modal-container ${props.visible ? 'visible' : 'hidden'}`} onClick={props.closeModal}>
+        <div className={'modal'}>
+            <div className={'close-modal'} onClick={props.closeModal}>×</div>
+            {props.children}
+        </div>
+    </div>
+)
+
+Modal.propTypes = {
+    visible: PropTypes.bool.isRequired,
+    closeModal: PropTypes.func.isRequired,
+    children: PropTypes.node.isRequired
+}
+
+export default Modal
